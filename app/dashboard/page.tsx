5e7b186b995b00d6a8f551366eef3df95ddc832b
Cristian David <cristiandavidvargas2@icloud.com>
2024-04-20 16:22:35 -0500

    Correction 4

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,11 @@
-
+"use server"
 import Link from 'next/link';
 import './styles.css'
 import DeleteButton from '../ui/DeleteButton/DeleteButton';
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
 
-export default async function Dashboard() {
-  const response = await fetch("https://apiappprospectos-production.up.railway.app/prospects")
+
+export default async function DashboardPage() {
+  const response = await fetch("https://apiappprospectos-production.up.railway.app/prospects", {cache:"no-store"})
   const prospectos = await response.json()
 
 
